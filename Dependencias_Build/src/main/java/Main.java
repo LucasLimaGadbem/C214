770854b1caf1869869935e89db45c82fbf410b0f
LucasLimaGadbem <lucaslimagadbem@gmail.com>
2025-08-12 23:13:29 -0300

    adicionando aula/atividade Dependencias_Build

---
+++ Dependencias_Build/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        // Data e hora atual
+        DateTime agora = new DateTime();
+        System.out.println("Data e hora atual: " + agora);
+
+        // Formatando a data
+        DateTimeFormatter formatador = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm:ss");
+        String dataFormatada = agora.toString(formatador);
+        System.out.println("Data e hora formatada: " + dataFormatada);
+
+        // Adicionando dias
+        DateTime daquiTresDias = agora.plusDays(1);
+        System.out.println("Amanhã será: " + daquiTresDias.toString(formatador));
+    }
+}
